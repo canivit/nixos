65001babca7e44a9fb98159f139d60b8465e9bbf
Can Ivit <canivit@users.noreply.github.com>
2022-09-06 09:13:48 -0400

    vscodium -> ms vscode + new extensions

--- roles/graphical/programs/vscode.nix
+++ roles/graphical/programs/vscode.nix
@@ -14,13 +14,21 @@ let
       version = "2.0.0";
       sha256 = "sha256-aBgTPB4bSKMjq4WmbZH+a96ljWUh6MpsNpKYtCNTzNM=";
     }
+
+    {
+      name = "dotnet-interactive-vscode";
+      publisher = "ms-dotnettools";
+      version = "1.0.3452021";
+      sha256 = "sha256-3AU0q6eKPC6/YURtlBCXa3BUU3HqpKI1n7VOOjHF/o4=";
+    }
+
   ];
 in
   {
     home-manager.users.can.programs.vscode = {
       enable = true;
     #package = pkgs.vscode-fhsWithPackages (ps: with ps; [ dotnet-sdk ]);
-    package = pkgs.vscodium;
+    package = pkgs.unstable.vscode;
 
     extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
@@ -30,7 +38,7 @@ in
       bbenoist.nix
       arrterian.nix-env-selector
       github.copilot
-      ms-dotnettools.csharp
+      pkgs.unstable.vscode-extensions.ms-dotnettools.csharp
     ] ++ marketExtensions;
 
     keybindings = [
