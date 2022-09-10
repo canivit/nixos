273faa2e806de7707d745e9d4925210d12714f53
Can Ivit <canivit@users.noreply.github.com>
2022-09-10 17:24:16 -0400

    use the flake.nix from the gnome branch

--- flake.nix
+++ flake.nix
@@ -3,18 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+
+    # use unstable channel for some packages
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/master";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }: 
+  let
+    system = "x86_64-linux";
+
+    overlay-unstable = final: prev: {
+      unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    };
+  in {
     nixosConfigurations.uranus = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
+
       modules = [
         {
           nixpkgs.config.allowUnfree = true;
+          nixpkgs.overlays = [ overlay-unstable ];
         }
 
         home-manager.nixosModules.home-manager {
