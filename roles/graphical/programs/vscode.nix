4cbf79e561f207108622b48ea3b14a2ab52484e2
Can Ivit <canivit@users.noreply.github.com>
2022-09-10 17:41:13 -0400

    use the vscode config from the gnome branch

--- roles/graphical/programs/vscode.nix
+++ roles/graphical/programs/vscode.nix
@@ -7,13 +7,28 @@ let
       version = "11.12.0";
       sha256 = "sha256-jyc6m6lfwSYt5xg2H5TGdxheKptho87C7eN4xuU5IYg=";
     }
+
+    {
+      name = "cake-vscode";
+      publisher = "cake-build";
+      version = "2.0.0";
+      sha256 = "sha256-aBgTPB4bSKMjq4WmbZH+a96ljWUh6MpsNpKYtCNTzNM=";
+    }
+
+    {
+      name = "dotnet-interactive-vscode";
+      publisher = "ms-dotnettools";
+      version = "1.0.3452021";
+      sha256 = "sha256-3AU0q6eKPC6/YURtlBCXa3BUU3HqpKI1n7VOOjHF/o4=";
+    }
+
   ];
 in
   {
     home-manager.users.can.programs.vscode = {
       enable = true;
     #package = pkgs.vscode-fhsWithPackages (ps: with ps; [ dotnet-sdk ]);
-    package = pkgs.vscodium;
+    package = pkgs.unstable.vscode;
 
     extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
@@ -23,7 +38,7 @@ in
       bbenoist.nix
       arrterian.nix-env-selector
       github.copilot
-      #vscode-extensions.ms-dotnettools.csharp
+      pkgs.unstable.vscode-extensions.ms-dotnettools.csharp
     ] ++ marketExtensions;
 
     keybindings = [
@@ -49,8 +64,8 @@ in
     ];
 
     userSettings = {
-      "window.titleBarStyle" = "native";
-      #"window.titleBarStyle" = "custom";
+      #"window.titleBarStyle" = "native";
+      "window.titleBarStyle" = "custom";
       "workbench.colorTheme" = "One Dark";
       "workbench.iconTheme" = "vscode-icons";
       "editor.minimap.enabled" = true;
