969907ab99fab62b92f046e7d740b2e169095e69
Can Ivit <canivit@users.noreply.github.com>
2022-10-01 18:36:45 -0400

    copy ssh config from main

--- roles/standard/ssh.nix
+++ roles/standard/ssh.nix
@@ -1,12 +1,44 @@
-{ ... }:
+{ config, ... }:
+let
+  home = config.users.users.can.home;
+in
 {
   home-manager.users.can.programs.ssh = {
     enable = true;
     matchBlocks = {
       "github.com" = {
         hostname = "github.com";
-        identityFile = "/home/can/.ssh/github";
+        identityFile = "${home}/.ssh/github";
         identitiesOnly = true;
+      };
+    };
+
+    matchBlocks = {
+      "github.khoury.northeastern.edu" = {
+        hostname = "github.khoury.northeastern.edu";
+        identityFile = "${home}/.ssh/github-khoury";
+        identitiesOnly = true;
+      };
+    };
+    
+    matchBlocks = {
+      "khoury" = {
+        hostname = "login.ccs.neu.edu";
+        port = 22;
+        user = "canivit";
+        identityFile = "${home}/.ssh/khoury-server";
+        identitiesOnly = true;
+      };
+    };
+
+    matchBlocks = {
+      "warhead" = {
+        hostname = "warhead.ccs.neu.edu";
+        port = 10001;
+        user = "hacker07";
+        identityFile = "${home}/.ssh/warhead";
+        identitiesOnly = true;
+        proxyJump = "khoury";
       };
     };
   };
