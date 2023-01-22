fd46e3e3c12491d08d331d2a13490ed6c842bc08
Can Ivit <canivit@users.noreply.github.com>
2023-01-22 07:59:54 -0500

    setup vscode for ruby

--- roles/graphical/programs/vscode.nix
+++ roles/graphical/programs/vscode.nix
@@ -22,13 +22,26 @@ let
       sha256 = "sha256-3AU0q6eKPC6/YURtlBCXa3BUU3HqpKI1n7VOOjHF/o4=";
     }
 
+    {
+      name = "Ruby";
+      publisher = "rebornix";
+      version = "0.28.1";
+      sha256 = "sha256-HAUdv+2T+neJ5aCGiQ37pCO6x6r57HIUnLm4apg9L50=";
+    }
+
+    {
+      name = "vscode-ruby";
+      publisher = "wingrunr21";
+      version = "0.28.0";
+      sha256 = "sha256-H3f1+c31x+lgCzhgTb0uLg9Bdn3pZyJGPPwfpCYrS70=";
+    }
   ];
 in
   {
     home-manager.users.can.programs.vscode = {
       enable = true;
     #package = pkgs.vscode-fhsWithPackages (ps: with ps; [ dotnet-sdk ]);
-    package = pkgs.unstable.vscode;
+    #package = pkgs.unstable.vscode;
 
     extensions = with pkgs.vscode-extensions; [
       vscodevim.vim
@@ -38,8 +51,8 @@ in
       bbenoist.nix
       arrterian.nix-env-selector
       github.copilot
-      pkgs.unstable.vscode-extensions.ms-dotnettools.csharp
-      pkgs.unstable.vscode-extensions.ms-vscode.cpptools
+      #pkgs.unstable.vscode-extensions.ms-dotnettools.csharp
+      #pkgs.unstable.vscode-extensions.ms-vscode.cpptools
       matklad.rust-analyzer
     ] ++ marketExtensions;
 
@@ -66,8 +79,8 @@ in
     ];
 
     userSettings = {
-      #"window.titleBarStyle" = "native";
-      "window.titleBarStyle" = "custom";
+      "window.titleBarStyle" = "native";
+      #"window.titleBarStyle" = "custom";
       "workbench.colorTheme" = "One Dark";
       "workbench.iconTheme" = "vscode-icons";
       "editor.minimap.enabled" = true;
@@ -102,6 +115,15 @@ in
       #"[csharp]" = {
       #  "editor.defaultFormatter" = "ms-dotnettools.csharp";
       #};
+
+      "ruby.useBundler" = true;
+      "ruby.useLanguageServer" = false;
+      "ruby.lint" = {
+        "rubocop" = {
+          "useBundler" = true;
+        };
+      };
+      "ruby.format" = "rubocop";
     };
   };
 }
