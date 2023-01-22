aba7424ae20da81279dbc16719c16692ba421c67
Can Ivit <canivit@users.noreply.github.com>
2023-01-22 08:01:30 -0500

    change monitor setup

--- roles/graphical/programs/i3/toggle_monitor.nix
+++ roles/graphical/programs/i3/toggle_monitor.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 let
   toggle_monitor = pkgs.writeShellScriptBin "toggle_monitor" ''
-    if xrandr | grep "DP-1-3 connected"; then
-      xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal
+    if xrandr | grep "DP-1 connected"; then
+      xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal
     else
-      xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-3 --off
+      xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off
     fi
   '';
 in
