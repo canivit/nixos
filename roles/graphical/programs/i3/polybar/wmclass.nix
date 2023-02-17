69b1b99d487a47aabfccb34afe8264beea096b63
Can Ivit <canivit@users.noreply.github.com>
2023-02-16 21:36:43 -0500

    fix wmclass not showing desktop correctly

--- roles/graphical/programs/i3/polybar/wmclass.nix
+++ roles/graphical/programs/i3/polybar/wmclass.nix
@@ -1,12 +1,8 @@
 { writeShellScriptBin, xprop, xdotool, gawk, gnused }:
 writeShellScriptBin "wmclass" ''
-  WM_DESKTOP=$(${xdotool}/bin/xdotool getwindowfocus)
-
-  if [ $WM_DESKTOP == "14680164" ]; then
-    echo "Desktop"
-
+  if ACTIVE_WINDOW=$(${xdotool}/bin/xdotool getactivewindow 2> /dev/null); then
+    echo $(${xprop}/bin/xprop -id $ACTIVE_WINDOW WM_CLASS | ${gawk}/bin/awk 'NF {print $NF}' | ${gnused}/bin/sed 's/"/ /g')
   else
-    WM_CLASS=$(${xprop}/bin/xprop -id $(${xdotool}/bin/xdotool getactivewindow) WM_CLASS | ${gawk}/bin/awk 'NF {print $NF}' | ${gnused}/bin/sed 's/"/ /g')
-    echo $WM_CLASS
+    echo "Desktop"
   fi
 ''
