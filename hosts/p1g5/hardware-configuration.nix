440d55e8aa4e42bdf846212110857258d9ac97c7
Can Ivit <canivit@users.noreply.github.com>
2023-02-18 11:39:29 -0500

    update kernel modules

--- hosts/p1g5/hardware-configuration.nix
+++ hosts/p1g5/hardware-configuration.nix
@@ -5,7 +5,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
