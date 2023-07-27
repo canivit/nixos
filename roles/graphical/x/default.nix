830583548fd03b1a937a05ffdf466c1f7a94920b
Can Ivit <canivit@users.noreply.github.com>
2023-07-26 22:38:21 -0400

    fix qt hidpi

--- roles/graphical/x/default.nix
+++ roles/graphical/x/default.nix
@@ -63,11 +63,15 @@ in
       "GDK_SCALE" = "2";
       "XCURSOR_SIZE" = "48";
       "GDK_DPI_SCALE" = "0.5";
-      "QT_SCALE_FACTOR" = "1.5";
+      "QT_SCALE_FACTOR" = "1";
+      "QT_AUTO_SCREEN_SCALE_FACTOR" = "1";
+      "QT_FONT_DPI" = "192";
     } else {
       "GDK_SCALE" = "1";
       "XCURSOR_SIZE" = "24";
       "GDK_DPI_SCALE" = "1";
-      "QT_SCALE_FACTOR" = "1.5";
+      "QT_SCALE_FACTOR" = "1";
+      "QT_AUTO_SCREEN_SCALE_FACTOR" = "1";
+      "QT_FONT_DPI" = "96";
     };
 }
